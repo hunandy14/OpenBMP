d89a73d05327516256336a326d8781b913f2643e
CharlotteHonG <hunandy14@gmail.com>
2018-04-30 15:54:36 +0800

    修改順序

--- OpenBMP/OpenBMP/OpenBMP.cpp
+++ OpenBMP/OpenBMP/OpenBMP.cpp
@@ -15,45 +15,6 @@ Final: 2017/06/14
 using namespace std;
 using uch = unsigned char;
 //----------------------------------------------------------------
-// 寫 Bmp 檔
-void OpenBMP::bmpWrite( string name, const vector<uch>& raw,
-	uint32_t width, uint32_t height, uint16_t bits)
-{
-	// 檔案資訊
-	BmpFileHeader file_h(width, height, bits);
-	// 圖片資訊
-	BmpInfoHeader info_h(width, height, bits);
-	// 寫檔
-	ofstream bmp(name, ios::binary);
-	bmp.exceptions(ifstream::failbit|ifstream::badbit);
-	bmp << file_h << info_h;
-	// 寫調色盤
-	if(bits == 8) {
-		for(unsigned i = 0; i < 256; ++i) {
-			bmp << uch(i) << uch(i) << uch(i) << uch(0);
-		}
-	}
-	// 寫入圖片資訊
-	size_t realW = info_h.biWidth * info_h.biBitCount/8.0;
-	size_t alig = (realW*3) % 4;
-
-
-	for(int j = height-1; j >= 0; --j) {
-		for(unsigned i = 0; i < width; ++i) {
-			if(bits==24) {
-				bmp << raw[(j*width+i)*3 + 2];
-				bmp << raw[(j*width+i)*3 + 1];
-				bmp << raw[(j*width+i)*3 + 0];
-			} else if(bits==8) {
-				bmp << raw[(j*width+i)];
-			}
-		}
-		// 對齊4byte
-		for(unsigned i = 0; i < alig; ++i) {
-			bmp << uch(0);
-		}
-	}
-}
 // 讀 Bmp 檔案
 void OpenBMP::bmpRead(vector<uch>& raw, string name,
 	uint32_t* width, uint32_t* height, uint16_t* bits) {
@@ -96,13 +57,46 @@ void OpenBMP::bmpRead(vector<uch>& raw, string name,
 		bmp.seekg(alig, ios::cur); // 跳開 4bite 對齊的空格
 	}
 }
-//----------------------------------------------------------------
-// 寫 Raw 檔
-void OpenBMP::rawWrite(std::string name, const std::vector<uch>& raw) {
-	std::ofstream raw_file(name.c_str(), std::ios::binary);
-	raw_file.exceptions(std::ifstream::failbit|std::ifstream::badbit);
-	raw_file.write(reinterpret_cast<const char*>(raw.data()), raw.size());
+// 寫 Bmp 檔
+void OpenBMP::bmpWrite( string name, const vector<uch>& raw,
+	uint32_t width, uint32_t height, uint16_t bits)
+{
+	// 檔案資訊
+	BmpFileHeader file_h(width, height, bits);
+	// 圖片資訊
+	BmpInfoHeader info_h(width, height, bits);
+	// 寫檔
+	ofstream bmp(name, ios::binary);
+	bmp.exceptions(ifstream::failbit|ifstream::badbit);
+	bmp << file_h << info_h;
+	// 寫調色盤
+	if(bits == 8) {
+		for(unsigned i = 0; i < 256; ++i) {
+			bmp << uch(i) << uch(i) << uch(i) << uch(0);
+		}
+	}
+	// 寫入圖片資訊
+	size_t realW = info_h.biWidth * info_h.biBitCount/8.0;
+	size_t alig = (realW*3) % 4;
+
+
+	for(int j = height-1; j >= 0; --j) {
+		for(unsigned i = 0; i < width; ++i) {
+			if(bits==24) {
+				bmp << raw[(j*width+i)*3 + 2];
+				bmp << raw[(j*width+i)*3 + 1];
+				bmp << raw[(j*width+i)*3 + 0];
+			} else if(bits==8) {
+				bmp << raw[(j*width+i)];
+			}
+		}
+		// 對齊4byte
+		for(unsigned i = 0; i < alig; ++i) {
+			bmp << uch(0);
+		}
+	}
 }
+//----------------------------------------------------------------
 // 讀 Raw 檔
 void OpenBMP::rawRead(std::vector<uch>& raw, std::string name) {
 	std::ifstream raw_file(name.c_str(), 
@@ -113,3 +107,10 @@ void OpenBMP::rawRead(std::vector<uch>& raw, std::string name) {
 	raw_file.read(reinterpret_cast<char*>(raw.data()), raw.size());
 	raw_file.close();
 }
+// 寫 Raw 檔
+void OpenBMP::rawWrite(std::string name, const std::vector<uch>& raw) {
+	std::ofstream raw_file(name.c_str(), std::ios::binary);
+	raw_file.exceptions(std::ifstream::failbit|std::ifstream::badbit);
+	raw_file.write(reinterpret_cast<const char*>(raw.data()), raw.size());
+}
+//----------------------------------------------------------------
